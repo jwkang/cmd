d9879aca82f3ab3a0c49316bc1d2b43fb72762e1
jongwon kang <krnomad@gmail.com>
2018-06-24 12:52:33 +0900

    [cmd_project] add my-sessions.sh and gitignore

---
+++ my-sessions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Bash Menu Script
+
+# 5122 ~ 5150 : ssh for docker
+
+PS3='Please enter your choice: '
+options=("KRNOMAD-NAS" "ubuntu14.04" "Option 3" "Quit")
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        ${options[0]})
+	    ssh krnomad@krnomad.asuscomm.com -p 5022
+	    exit
+            ;;
+        ${options[1]})
+	    ssh jwkang2@krnomad.asuscomm.com -p 5122
+            exit
+            ;;
+        "Option 3")
+            echo "you chose choice 3"
+            exit
+            ;;
+	"mount to NAS")
+	    mount_smbfs //krnomad@krnomad-nas/SSD/ nas
+	    ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
