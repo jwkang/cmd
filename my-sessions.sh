e92b5bc660a23dac2825dfbe89d4ff0a0877d592
jongwon kang <krnomad@gmail.com>
2018-06-24 15:03:28 +0900

    [sessions] add new host information (naver claud platform)

--- my-sessions.sh
+++ my-sessions.sh
@@ -4,7 +4,7 @@
 # 5122 ~ 5150 : ssh for docker
 
 PS3='Please enter your choice: '
-options=("KRNOMAD-NAS" "ubuntu14.04" "Option 3" "Quit")
+options=("KRNOMAD-NAS" "ubuntu14.04" "Naver claud platform" "Option 3" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -16,6 +16,10 @@ do
         ${options[1]})
 	    ssh jwkang2@krnomad.asuscomm.com -p 5122
             exit
+            ;;
+        ${options[2]})
+        ssh jwkang2@106.10.46.141
+        exit
             ;;
         "Option 3")
             echo "you chose choice 3"
